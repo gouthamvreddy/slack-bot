6f45499cfb1742fa3fff8601a97ceeea37ec99f4
Goutham Reddy <gouthamvreddy@gmail.com>
2016-04-12 22:33:14 -0700

    updated bot to respond to any messages posted to channel

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+
 var Slack = require('@slack/client');
 var RtmClient = Slack.RtmClient;
 var RTM_EVENTS = Slack.RTM_EVENTS;
 
-var token = process.env.SLACK_TOKEN || '';
+var TOKEN = process.env.API_TOKEN || '';
 
-var rtm = new RtmClient(token, { logLevel: 'info' });
+var rtm = new RtmClient(TOKEN, { logLevel: 'info' });
 
 rtm.start();
 
-rtm.on(RTM_EVENTS.REACTION_ADDED, function(message) {
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
   var channel = message.channel;
   var text = message.text;
   console.log(channel);
   console.log(message);
-  rtm.sendMessage(text, "C0ZPHKNDP");
+  rtm.sendMessage(text, channel);
 });
